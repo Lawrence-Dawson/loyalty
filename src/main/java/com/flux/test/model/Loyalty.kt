4d69feb33024702901277cf67798e179556e9de0
Lawrence Dawson <ld@mettrr.com>
2019-05-10 11:38:19 +0100

    create merchant with merchants own schemes

--- src/main/java/com/flux/test/model/Loyalty.kt
+++ src/main/java/com/flux/test/model/Loyalty.kt
@@ -3,9 +3,26 @@ import com.flux.test.ImplementMe
 
 class Loyalty(schemes: List<Scheme>) : ImplementMe {
     override var schemes = schemes
+    val merchants = mutableListOf<Merchant>()
 
     override fun apply(receipt: Receipt): List<ApplyResponse> {
-        val scheme = this.schemes.first()
-        return listOf(ApplyResponse(scheme.id, 1, 1, listOf()))
+        val merchant = this.getMerchant(receipt.merchantId)
+        
+        return listOf(ApplyResponse(schemes.first().id, 1, 1, listOf()))
     }
+
+    private fun getMerchant(merchantId: MerchantId): Merchant {
+        if (merchants.any { m -> m.id == merchantId }) {
+            return merchants.filter { m -> m.id == merchantId }.first()
+        }
+
+        val merchantSchemes = schemes.filter { scheme -> scheme.merchantId == merchantId }
+        val merchant = Merchant(merchantId, merchantSchemes)
+
+        merchants.add(merchant)
+
+        return merchant
+    }
+
+
 }
---
+++ src/main/java/com/flux/test/model/Merchant.kt
@@ -0,0 +1,3 @@
+package com.flux.test.model
+
+class Merchant(val id: MerchantId, var schemes: List<Scheme>)
